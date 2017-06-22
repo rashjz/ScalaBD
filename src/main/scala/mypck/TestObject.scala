5c7fa512041d94ff893d6e71aa3c73718086bc5b
Rashad <rashadjavad@gmail.com>
2017-06-22 22:02:36 +0200

    scala learning 1

--- src/main/scala/mypck/TestObject.scala
+++ src/main/scala/mypck/TestObject.scala
@@ -1,9 +1,10 @@
 package mypck
 
+import scala.collection.mutable.ArrayBuffer
 import scala.math._;
 
 /**
-  * Created by rasha_000 on 6/20/2017.
+  * Rashad Javadov
   */
 object TestObject extends App {
 
@@ -22,5 +23,35 @@ object TestObject extends App {
       print(i)
       i += 1
     }
+
+
+    def getsum(x: Int = 1, y: Int = 1): Int = return x + y
+
+    print("\n getsum is " + getsum(12, 33))
+
+    //play with string array
+    val employees = ArrayBuffer[String]()
+    employees.insert(0, "Rashad")
+    employees.insert(1, "Malik")
+    print("\n " + employees)
+
+    //for each style
+    for (emp <- employees)
+      print("\n Employee : " + emp)
+
+    //playing with  array of int
+    val ages = new Array[Int](9)
+    for (j <- 0 to (ages.length - 1)) {
+      ages(j) = j
+      print("\n age is " + ages(j))
+    }
+
+    //int array all multiply 2
+    val ageMult2 = for (myage <- ages) yield 2 * myage
+    ageMult2.foreach(println)
+    
+    //find values divided to 4
+    val ageDiv4 = for (myage <- ages if myage % 4 == 0) yield myage
+    ageDiv4.foreach(println)
   }
 }
