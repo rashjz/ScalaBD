41f907614ce874b958973e209782851fd5ad471c
Rashad <rashadjavad@gmail.com>
2017-06-23 22:46:55 +0200

    messing with objects

---
+++ src/main/scala/mypck/Animal.scala
@@ -0,0 +1,42 @@
+package mypck
+
+/**
+  * Created by rasha_000 on 6/23/2017.
+  */
+class Animal() {
+  var id: Int = 0
+  var name: String = ""
+  var sound: String = ""
+
+  def Animal(): Unit = {
+    this.id = 0
+    this.name = ""
+    this.sound = ""
+  }
+
+  def getId(): Int = id
+
+  def getName(): String = name
+
+  def getSound: String = sound
+
+  def setId(id: Int) {
+    this.id = id
+  }
+
+  def setName(name: String) {
+    if (name.matches(".*\\d+.*"))
+      this.name = name
+    else
+      this.name = "No name"
+  }
+
+  def setSound(sound: String) {
+    this.sound = sound
+  }
+
+  override def toString: String = {
+    return " %s with the id %d say %s".format(this.name, this.id,this.sound)
+  }
+
+}
---
+++ src/main/scala/mypck/DomainMainRunner.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by rasha_000 on 6/23/2017.
+  */
+package mypck
+
+object DomainMainRunner {
+
+  def main(args: Array[String]): Unit = {
+    val dog = new Animal()
+    dog.setId(0)
+    dog.setSound("Bark")
+    dog.setName("Bob1")
+
+    print(dog.toString)
+  }
+}
