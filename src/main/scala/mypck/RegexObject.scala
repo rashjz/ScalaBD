a0ed9efe4a393408dab73f124ef02cdf5b748e53
Rashad <rashadjavad@gmail.com>
2017-06-25 20:59:33 +0200

    week1

---
+++ src/main/scala/mypck/RegexObject.scala
@@ -0,0 +1,15 @@
+package mypck
+
+/**
+  * Created by rasha_000 on 6/25/2017.
+  */
+object RegexObject {
+  def main(args: Array[String]) {
+    val pattern = "(S|s)cala".r
+    val str = "Scala is Scalable and cool"
+
+    println(pattern findFirstIn str)
+    println("\n"+(pattern findAllIn str).mkString(","))
+    println("\n"+(pattern replaceAllIn (str, "Java")))
+  }
+}
